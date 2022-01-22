2ce3beabc119b5994f7c9bb1173a0da256987903
Johan Edblom <johane92@gmail.com>
2022-01-22 15:05:12 +0100

    Added Week2 assignment

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
 import pandas
+import numpy as np
+
+a = np.array([1,2,3,4])
+print(a)
+
+import time 
+
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
+
+c = np.dot(a,b)
+
+a = np.random.randn(4,3)
+b = np.random.randn(3,2)
+c = a*b
+c.shape
